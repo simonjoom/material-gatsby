1be694958f666cbf9b2635ed3e7f70f43d4ec49a
William Imoh <williamimoh@yahoo.com>
2018-09-15 23:48:19 +0100

    style carousel complete

--- src/components/FrontCarousel/FrontCarousel.js
+++ src/components/FrontCarousel/FrontCarousel.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Carousel} from "react-responsive-carousel";
-// import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'react-responsive-carousel/lib/styles/carousel.css';
 import image01 from '../../images/index/2048a.jpg'
 import image02 from '../../images/index/exp.jpg'
@@ -9,34 +8,43 @@ import image04 from '../../images/index/nice_dog.jpg'
 import image05 from '../../images/index/offpiste.jpg'
 import image06 from '../../images/index/snowboard.jpg'
 import image07 from '../../images/index/top.jpg'
-// let Carousel = require('react-responsive-carousel').Carousel;
+import './FrontCarousel.scss'
 
  const FrontCarousel = ()=>{
     return (
         <div className="carousel">
-            <Carousel autoPlay infiniteLoop width="500px" className="carousel-main">
-                <div>
-                    <img src={image01} alt=""/>
-                </div>
-                <div>
-                    <img src={image02} alt=""/>
-                </div>
-                <div>
-                    <img src={image03} alt=""/>
-                </div>
-                <div>
-                    <img src={image04} alt=""/>
-                </div>
-                <div>
-                    <img src={image05} alt=""/>
-                </div>
-                <div>
-                    <img src={image06} alt=""/>
-                </div>
-                <div>
-                    <img src={image07} alt=""/>
-                </div>
-            </Carousel>
+            <div className="carousel-container">
+                <Carousel 
+                    autoPlay 
+                    infiniteLoop 
+                    className="carousel-main"
+                    showStatus={false}
+                    showThumbs={false}
+                >
+                    <div>
+                        <img src={image01} alt=""/>
+                    </div>
+                    <div>
+                        <img src={image02} alt=""/>
+                    </div>
+                    <div>
+                        <img src={image03} alt=""/>
+                    </div>
+                    <div>
+                        <img src={image04} alt=""/>
+                    </div>
+                    <div>
+                        <img src={image05} alt=""/>
+                    </div>
+                    <div>
+                        <img src={image06} alt=""/>
+                    </div>
+                    <div>
+                        <img src={image07} alt=""/>
+                    </div>
+                </Carousel>
+            </div>
+            
         </div> 
     )
 }
