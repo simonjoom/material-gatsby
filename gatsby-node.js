6d1c36c908b82cbc00be4ba21b9bde79e47ad8de
simon <simon@skiscool.com>
2018-09-16 22:23:03 +0200

    deps

--- gatsby-node.js
+++ gatsby-node.js
@@ -359,3 +359,17 @@ exports.onPostBootstrap = () => {
     path.join(__dirname, "/public/locales")
   );
 };
+
+exports.onPostBuild = function(args, options) {
+  var filename = 'sw.js';
+
+  var read = fs.createReadStream(path.join(__dirname, 'sw.js'))
+  var write = fs.createWriteStream(path.resolve('public', filename));
+
+  return new Promise(function(resolve, reject) {
+    var stream = read.pipe(write);
+
+    stream.once('finish', resolve);
+    stream.once('error', reject);
+  });
+};
