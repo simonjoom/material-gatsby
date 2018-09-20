e392795e89e543511fd77758fdd8603926c4430c
simon <simon@skiscool.com>
2018-09-20 17:16:17 +0200

    test netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,7 @@ module.exports = {
       }
     },
     "gatsby-transformer-sharp",
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-sharp",
     "gatsby-plugin-catch-links",
     "gatsby-plugin-twitter",
