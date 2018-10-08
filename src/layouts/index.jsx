7033bd22d130d4303bacb0562e4453b4dbf8271b
William Imoh <williamimoh@yahoo.com>
2018-10-08 20:46:36 +0100

    commit before pull

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,13 +3,9 @@ import "font-awesome/scss/font-awesome.scss";
 import "./index.scss";
 import "./global.scss";
 import "./toolbar.scss";
-<<<<<<< HEAD
 import "./carousel.scss";
 import Chat from "../components/Chat/Chat"
 //import Paper from "react-md/lib/Papers/Paper";
-=======
-import "./carousel.scss"; 
->>>>>>> 8d315dab10218e76f03e303c536abfe94d94aeec
 import Navigation from "../components/Navigation";
 import config from "../../data/SiteConfig"; 
 
