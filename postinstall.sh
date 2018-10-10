387fa7ff909771dd866f0fab9a286086be4916b5
simon <simon@skiscool.com>
2018-10-10 02:11:55 +0200

    chat + fixes inferno

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,8 @@ export NOYARNPOSTINSTALL="yes"
 cp -Rf ./packhack/@reach ./node_modules/
 # cp -Rf ./packhack/react-dev-utils ./node_modules/
 # cp -Rf ./packhack/react-native-web ./node_modules/
-cp -Rf ./packhack/gatsby ./node_modules/ 
+cp -Rf ./packhack/gatsby ./node_modules/
+cp -Rf ./packhack/inferno-compat ./node_modules/ 
 cp -Rf ./packhack/gatsby-plugin-sass ./node_modules/ 
 cp -Rf ./packhack/htmltojsx ./node_modules/ 
 cp -Rf ./packhack/gatsby-image ./node_modules/ 
