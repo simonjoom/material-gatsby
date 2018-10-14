21f943b2ab75cb0b7ff10568ec5a03303dbf6bd1
simon <simon@skiscool.com>
2018-10-14 16:03:40 +0200

    remove backline

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Button from "../reactLIB/Button";
 
-const ButtonC = ({ cls, children,flat, ...other }) => (
-  <Button className={cls} flat={flat} {...other}>
-    {children}
-  </Button>
-);
+const ButtonC = ({ cls, children, flat, ...other }) => {
+  const cc = children.filter(a => a !== String.fromCharCode(10));
+  return (
+    <Button className={cls} flat={flat} {...other}>
+      {cc}
+    </Button>
+  );
+};
 
 export default ButtonC;
