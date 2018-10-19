136dca2599dd0f9d001ce8f9bfa7c5ca4764dbc0
simon <simon@skiscool.com>
2018-10-19 15:28:43 +0200

    Testable chat animation

--- src/chatcomponents/chat/ChatsPage.js
+++ src/chatcomponents/chat/ChatsPage.js
@@ -7,12 +7,12 @@ import { AUTH_TOKEN } from "../../constants/constants";
 class ChatsPage extends Component {
   state = {
     orderBy: "createdAt_ASC",
-    openChat: true
+    addcss: ""
   };
-  /* componentWillReceiveProps(props) {
-    console.log("props" ,props)
-    this.setState({ openChat: props.openChat });
-  }*/
+  componentDidMount(props) {
+    this.setState({ addcss: " itc-cvs itc-messenger-body-view-enter-done" });
+  }
+
   render() {
     const authToken = localStorage.getItem(AUTH_TOKEN);
     console.log("ChatsPage");
@@ -20,15 +20,17 @@ class ChatsPage extends Component {
       return <NotAuth />;
     }
     return (
-      <div className="itc-messenger-body">
+      <div className={`itc-messenger-body${this.state.addcss}`}>
         <div className="itc-cvs-body">
-          <div className="itc-cvs-body-parts">
+          <div className="itc-cvs-body-parts" id="listChats">
             <div className="itc-cvs-parts-wrapper">
               <div
                 className="itc-cvs-parts md-grid"
                 style={{ margin: 0, padding: 0, marginTop: 5 }}
               >
-                <ChatsPageList orderBy={this.state.orderBy} />
+                {this.state.addcss !== "" && (
+                  <ChatsPageList orderBy={this.state.orderBy} />
+                )}
               </div>
             </div>
           </div>
--- src/chatcomponents/nav/layout/SideBar.jsx
+++ src/chatcomponents/nav/layout/SideBar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ListSideBar from "./ListSideBar";
-import { SideBarContext } from "../../SideBarContext";
 
-const SideBar = () => (
-  <SideBarContext.Consumer>
-    {context =>
-      context.state.isSideBarOpen && (
-        <div className="sidechat md-cell md-cell--1 md-cell--1-tablet md-cell--1-phone" tabIndex={0} role="button">
-          <ListSideBar
-            isMobile={context.state.isMobile}
-            role={context.Me.role}
-          />
-        </div>
-      )
-    }
-  </SideBarContext.Consumer>
+//import { SideBarContext } from "../../SideBarContext";
+
+const SideBar = ({ isSideBarOpen,isMobile,Me}) => (
+  <>
+    {isSideBarOpen && (
+      <div
+        className="sidechat md-cell md-cell--1 md-cell--1-tablet md-cell--1-phone"
+        tabIndex={0}
+        role="button"
+      >
+        <ListSideBar isMobile={isMobile} role={Me.role} />
+      </div>
+    )}
+  </>
 );
 
 export default SideBar;
